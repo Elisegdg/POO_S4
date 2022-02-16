5d4c3941b5fec6f75e5930d52dcfc7951fee04ad
Elise Gondange <elise@MacBook-Pro-de-Elise.local>
2022-02-16 17:18:06 +0100

    [COMMON] reformating fonctions

--- src/Common/get_input_from_user.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int get_int_from_user()
-{
-    int numberPicked;
-    std::cin >> numberPicked;
-    return numberPicked;
-    // TODO ;) Use std::cin to get an int, and then return it
-}
--- src/Common/get_input_from_user.hpp
+++ src/Common/get_input_from_user.hpp
@@ -1,1 +1,10 @@
-int get_int_from_user();
+#include <iostream>
+
+template<typename T>
+T get_input_from_user()
+{
+    T inputPicked;
+    std::cin >> inputPicked;
+    return inputPicked;
+    // TODO ;) Use std::cin to get an int, and then return it
+}
