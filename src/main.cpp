6ac38951cc3b2ea8257ef26d687f02e9214355f1
Elise Gondange <elise@MacBook-Pro-de-Elise.local>
2022-05-18 17:08:55 +0200

    [MENU] menu added

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Common/get_input_from_user.hpp"
 #include "Guess_the_number/play_guess_the_number.hpp"
 #include "Hangman/play_hangman.hpp"
 
@@ -11,8 +12,25 @@ void show_the_list_of_commands()
 
 int main()
 {
-    play_hangman();
-    // play_guess_the_number();
-
-    // std::cout << pick_a_random_word() << std::endl;
+    bool quit = false;
+    while (!quit) {
+        show_the_list_of_commands();
+        std::cout << "Choose your game" << std::endl;
+        const auto command = get_input_from_user<char>();
+        switch (command) {
+        case '1':
+            play_guess_the_number();
+            break;
+        case '2':
+            play_hangman();
+            break;
+        case 'q':
+            quit = true;
+            break;
+        default:
+            std::cout << "Invalid command" << std::endl;
+            break;
+        }
+    }
+    return 0;
 }
