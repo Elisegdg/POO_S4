cbd130691062b587a124552bdfce570c25487df9
Elise Gondange <elise@MacBook-Pro-de-Elise.local>
2022-05-18 17:00:08 +0200

    [MENU] add function show_list_of_games

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,13 @@
 #include "Guess_the_number/play_guess_the_number.hpp"
 #include "Hangman/play_hangman.hpp"
 
+void show_the_list_of_commands()
+{
+    std::cout << "List of the games" << std::endl;
+    std::cout << "1 - Guess the number" << std::endl;
+    std::cout << "2 - Play hangman" << std::endl;
+}
+
 int main()
 {
     play_hangman();
